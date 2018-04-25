99a4d473182ba06df67bd850196fc0db1e898653
thegodone <guillaume.godin@gmail.com>
2018-04-25 14:26:31 +0200

    add cuda in nnutils

--- jtnn/nnutils.py
+++ jtnn/nnutils.py
@@ -5,9 +5,9 @@ from torch.autograd import Variable
 
 def create_var(tensor, requires_grad=None):
     if requires_grad is None:
-        return Variable(tensor)
+        return Variable(tensor).cuda()
     else:
-        return Variable(tensor, requires_grad=requires_grad)
+        return Variable(tensor, requires_grad=requires_grad).cuda()
 
 def index_select_ND(source, dim, index):
     index_size = index.size()
@@ -19,7 +19,7 @@ def index_select_ND(source, dim, index):
 def GRU(x, h_nei, W_z, W_r, U_r, W_h):
     hidden_size = x.size()[-1]
     sum_h = h_nei.sum(dim=1)
-    z_input = torch.cat([x,sum_h], dim=1)
+    z_input = torch.cat([x,sum_h], dim=1).cuda()
     z = nn.Sigmoid()(W_z(z_input))
 
     r_1 = W_r(x).view(-1,1,hidden_size)
@@ -28,7 +28,7 @@ def GRU(x, h_nei, W_z, W_r, U_r, W_h):
     
     gated_h = r * h_nei
     sum_gated_h = gated_h.sum(dim=1)
-    h_input = torch.cat([x,sum_gated_h], dim=1)
+    h_input = torch.cat([x,sum_gated_h], dim=1).cuda()
     pre_h = nn.Tanh()(W_h(h_input))
     new_h = (1.0 - z) * sum_h + z * pre_h
     return new_h
